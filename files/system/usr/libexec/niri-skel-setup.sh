acb93d590978080c3edc76164c456ed1b9abb0b4
jflores <jflores@aits-158920.local>
2026-09-04 16:45:41 -0500

    Add kitty config with noctalia theme sync, enable kitty template

--- files/system/usr/libexec/niri-skel-setup.sh
+++ files/system/usr/libexec/niri-skel-setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Copy default noctalia config from /etc/skel on first login
+# Copy default configs from /etc/skel on first login
 # (niri config lives at /etc/niri/config.kdl, applied system-wide)
 
 SKEL_DIR="/etc/skel/.config"
 CONFIG_DIR="$HOME/.config"
 
-app="noctalia"
-src="$SKEL_DIR/$app"
-dest="$CONFIG_DIR/$app"
-if [ -d "$src" ] && [ ! -d "$dest" ]; then
-    cp -r "$src" "$dest"
-fi
+for app in noctalia kitty; do
+    src="$SKEL_DIR/$app"
+    dest="$CONFIG_DIR/$app"
+    if [ -d "$src" ] && [ ! -d "$dest" ]; then
+        cp -r "$src" "$dest"
+    fi
+done
