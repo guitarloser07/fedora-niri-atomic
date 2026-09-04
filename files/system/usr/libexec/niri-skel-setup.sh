86e62d5560dcc162daaa1b294c8f338a655922f0
jflores <jflores@aits-158920.local>
2026-09-04 17:36:00 -0500

    Revert config changes: bar style, kitty theme sync, templates

--- files/system/usr/libexec/niri-skel-setup.sh
+++ files/system/usr/libexec/niri-skel-setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Copy default configs from /etc/skel on first login
+# Copy default noctalia config from /etc/skel on first login
 # (niri config lives at /etc/niri/config.kdl, applied system-wide)
 
 SKEL_DIR="/etc/skel/.config"
 CONFIG_DIR="$HOME/.config"
 
-for app in noctalia kitty; do
-    src="$SKEL_DIR/$app"
-    dest="$CONFIG_DIR/$app"
-    if [ -d "$src" ] && [ ! -d "$dest" ]; then
-        cp -r "$src" "$dest"
-    fi
-done
+app="noctalia"
+src="$SKEL_DIR/$app"
+dest="$CONFIG_DIR/$app"
+if [ -d "$src" ] && [ ! -d "$dest" ]; then
+    cp -r "$src" "$dest"
+fi
